437690d207ef6d6b115c832433ed677e1804280a
Rob Hill <hillrg22@gmail.com>
2019-07-16 12:18:12 -0600

    react routing setup

--- src/App.js
+++ src/App.js
@@ -2,24 +2,22 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
 
+import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
+
+import Home from './components/Home'
+import Navigation from './components/Navigation'
+import Products from './components/Products'
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Route path= '/' component={Navigation} />
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Route path='/products' component={Products} />
+      </Switch>
+
+    </Router>
   );
 }
 
---
+++ src/components/Products.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+const Products = () => {
+  return(
+    <div className='App'>
+      <header>
+        <h1>Welcome to Products List!</h1>
+      </header>
+    </div>
+  )
+}
+
+
+export default Products
