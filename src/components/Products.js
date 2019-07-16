0d1049a58a462ede4f3012d866f833a46deda4b0
Rob Hill <hillrg22@gmail.com>
2019-07-16 12:35:25 -0600

    fetching product list on client side

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,14 +1,34 @@
-import React from 'react'
+import React, {Component} from 'react'
 
 
-const Products = () => {
-  return(
-    <div className='App'>
-      <header>
-        <h1>Welcome to Products List!</h1>
-      </header>
-    </div>
-  )
+class Products extends Component {
+  state= {
+    products: []
+  }
+
+
+  componentDidMount() {
+    fetch('http://localhost:4000/api/products')
+      .then(res => res.json())
+      .then(products => {
+        this.setState({
+          products
+        })
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
+
+  render(){
+    return(
+      <div className='App'>
+        <header>
+          <h1>Welcome to Products List!</h1>
+        </header>
+      </div>
+    )
+  }
 }
 
 
