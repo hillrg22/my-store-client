bc239db65689367263402badcd46d0c08eda1539
Rob Hill <hillrg22@gmail.com>
2019-07-17 12:02:08 -0600

    auth0 integration and adding basic profile page

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,12 +1,26 @@
 import React from 'react'
-
 import { Link} from 'react-router-dom'
+import { useAuth0 } from '../react-auth0-wrapper'
+
 
 const Navigation = () => {
+  const { isAuthenticated, loginWithRedirect, logout } = useAuth0()
+
   return(
     <nav>
       <Link to='/'>Home</Link>
       <Link to='/products'>Products</Link>
+      {!isAuthenticated && (
+        <button
+          onClick={() =>
+            loginWithRedirect({})
+          }
+        >
+         Log in
+        </button>
+      )}
+     
+      {isAuthenticated && <button onClick={() => logout()}>Log out</button>}
     </nav>)
 }
 
